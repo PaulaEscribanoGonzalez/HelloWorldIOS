eb0db21225abd7f1cb81049cc0dce6ffdc63b1a8
Paula Escribano <paula.escribano@euneiz.com>
2025-10-14 19:52:10 +0200

    no message

---
+++ HelloWorld/HelloWorld/Birthday/BirthdayViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Student on 30/9/25.
+//
+
+import UIKit
+
+class BirthdayViewController: UIViewController {
+    
+    @IBOutlet weak var labelBirthday: UILabel!
+    @IBOutlet weak var labelWish: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.title = "Hello World"
+        
+        labelBirthday.text = "Happy Birthday Paula!"
+        labelWish.text = "I wish you the best"
+        
+        
+    }
+
+}
+
